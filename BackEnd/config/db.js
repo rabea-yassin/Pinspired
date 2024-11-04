992d6fd40b53db6921ba38bc38869393e02e637c
rabea-yassin <your-email@example.com>
2024-11-04 12:47:02 +0200

    Backend should be ready

--- BackEnd/config/db.js
+++ BackEnd/config/db.js
@@ -1,58 +1,17 @@
+require('dotenv').config();
 const mysql = require("mysql");
 
-// // Create connection
-// const db = mysql.createConnection({
-//   host: 'localhost',
-//   user: 'root',
-//   password: 'r1a2b3e4a5',
-//   database: 'pinterest'
-// });
-// // Connect
-// db.connect((err) => {
-//   if (err) {
-//     throw err;
-//   }
-//   console.log('MySql Connected...');
-// });
-
-
-// // Create connection
-// const db2 = mysql.createConnection({
-//   host: 'blw0snvjnx9eqnwpk721-mysql.services.clever-cloud.com',
-//   user: 'uepafc6b5lczfc03',
-//   password: 'PN49xR8xfMbhIB0sTZx6',
-//   database: 'blw0snvjnx9eqnwpk721'
-// });
-// // Connect
-// db2.connect((err) => {
-//   if (err) {
-//     throw err;
-//   }
-//   console.log('MySql Connected...');
-// });
-
-
-
-
-
-// pool connction
-
 const db_connection = mysql.createPool({
-  host: 'blw0snvjnx9eqnwpk721-mysql.services.clever-cloud.com',
-    user: 'uepafc6b5lczfc03',
-    password: 'PN49xR8xfMbhIB0sTZx6',
-    database: 'blw0snvjnx9eqnwpk721',
-    connectionLimit: 5,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  connectionLimit: 5,
 });
 
 db_connection.getConnection((err, connection) => {
   if (err) console.error(err);
   console.log('MySQL Connection Established: ', connection.threadId);
-})
+});
 
 module.exports = db_connection;
-
-
-// module.exports = db2;
-
-
